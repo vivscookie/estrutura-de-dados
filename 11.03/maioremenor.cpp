ace5c046e26248560f31f3ddfae78e01a08cc2aa
Vivs <viih_almada@hotmail.com>
2026-03-11 11:12:22 -0300

    Desafio maior e menor - incompleto

---
+++ 11.03/maioremenor.cpp
@@ -0,0 +1,36 @@
+//Encontre o menor e o maior elemento em um vetor de 6 elementos
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <stdio.h>
+#include <locale.h>
+
+
+using namespace std;
+
+int main(){
+	setlocale (LC_ALL, "Portuguese");
+	int n[6], maior, menor, i;
+	
+	maior = n[0];
+	menor = n[0];
+	
+	for(i=0;i<6;i++){
+		cout << "Digite número: ";
+		cin >> n[i];
+		
+		if (n[i]<menor){
+			menor=n[i];
+		}
+		
+		if (n[i]>maior){
+			maior=n[i];
+		}
+		
+		cout << "Menor elemento: " << menor;
+		cout << "Maior elemento: " << maior;
+	}
+
+	return 0;
+}
